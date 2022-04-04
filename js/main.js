979eff3db7044d0080908ef5cbc2233ae24c108a
Takoe <some@example.com>
2022-04-04 16:09:43 +0700

    form

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-
 const categoriesButton = document.querySelector('.app__categories-title');
-const categoriesList = document.querySelector('.app__categories-list');
 
-categoriesButton.addEventListener('click', e => {
-  // if(document.querySelectorAll('.header__item-dropdown.active')) {
-  //   document.querySelectorAll('.header__item-dropdown.active').forEach(elem => elem.classList.remove('active'));
-  // }
-
+categoriesButton.addEventListener('click', () => {
   categoriesButton.closest('.app__filter-categories').classList.add('active');
-  // button.closest('.header__item-dropdown').classList.add('open');
 });
 
 document.addEventListener('click', e => {
   if(!e.target.closest('.app__filter-categories')) {
     document.querySelector('.app__filter-categories.active').classList.remove('active');
   }
+});
+
+const selectTitle = document.querySelector('.app__select-title');
+const selectListItems = document.querySelectorAll('.app__select-list li');
+
+const selectClose = () => {
+  document.querySelector('.app__form-select.active').classList.remove('active');
+}
+
+selectTitle.addEventListener('click', () => {
+  selectTitle.closest('.app__form-select').classList.add('active');
+});
+
+document.addEventListener('click', e => {
+  if(!e.target.closest('.app__form-select')) {
+    selectClose();
+  }
+});
+
+selectListItems.forEach(item => {
+  item.addEventListener('click', () => {
+    selectTitle.innerHTML = item.innerHTML;
+    selectClose();
+  })
 })
